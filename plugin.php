59cc6f03285d3469790dee521a445b97d8fa8b14
Felix Arntz <flixos90@gmail.com>
2025-09-01 17:55:21 -0700

    Add wp-env and plugin.php file to easily test package in WordPress.

---
+++ plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: AI Client
+ * Plugin URI: https://github.com/WordPress/wp-ai-client/
+ * Description: An AI client and API for WordPress to communicate with any generative AI models of various capabilities using a uniform API.
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * Version: n.e.x.t
+ * Author: WordPress AI Team
+ * Author URI: https://make.wordpress.org/ai/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: wp-ai-client
+ *
+ * @package wp-ai-client
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
