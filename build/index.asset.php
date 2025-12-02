c598a66e1f9348d36dbf1810ce52661c99a96ebe
Felix Arntz <flixos90@gmail.com>
2025-12-01 16:11:17 -0800

    Fresh build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch'), 'version' => 'b3988d671061efe65590');
+<?php return array('dependencies' => array('wp-api-fetch'), 'version' => '1282f672408857f354a5');
